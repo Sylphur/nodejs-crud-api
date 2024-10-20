5dd999530716b0dbc7a46e2e13735a769a9e66c8
Sylphur <neosylphur@gmail.com>
2024-10-21 02:03:08 +0300

    feat: add nodemon, uuid, dotenv, ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+// import fs from 'fs';
+// import fsp from 'fs/promises';
+// import path from 'path';
+// import http from 'http';
+// import events from 'events';
+import 'dotenv/config';
+
 console.log('Hello world');
+console.log(process.env.PORT);
