8701ad5a53c7f0e9ada2e92a3e68e99c931aeb5f
Sylphur <neosylphur@gmail.com>
2024-10-22 02:48:48 +0300

    feat: add workers, balancer

---
+++ src/loadBalancer.ts
@@ -0,0 +1,15 @@
+import http from 'http';
+import 'dotenv/config';
+import { getWorkers } from './services/calculateWorkers';
+import { roundRobin } from './services/roundRobin';
+
+const PORT = process.env.PORT || 3500;
+const workers = getWorkers('localhost', +PORT);
+
+const servers = workers.map((server) => ({
+  ...server,
+}));
+const main = http.createServer((req, res) => {
+  roundRobin(servers, req, res);
+});
+main.listen(PORT, () => console.log('Load balancer is running on port ', PORT));
